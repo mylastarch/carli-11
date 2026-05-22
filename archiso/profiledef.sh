5ab7145a515187562cf1bc18939998f227054533
mylastarch <mylastarch@dccathome.com>
2026-05-22 16:43:44 -0400

    made some changes

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -15,6 +15,7 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 #bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
+  ["/root/.gnupg"]="0:0:400"
   ["/etc/shadow"]="0:0:400"
   ["/etc/gshadow"]="0:0:400"
   ["/root"]="0:0:750"
